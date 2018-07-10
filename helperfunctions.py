4950680ce6ada00527dddf092cc7854285b87950
shinyumh <40373356+shinyumh@users.noreply.github.com>
2018-07-10 10:08:42 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,11 @@
+def add(inlist):
+    return sum(inlist)
+
+def diff(inlist):
+    return max(inlist)-min(inlist)
+
+def product(inlist):
+    return inlist[0]*inlist[1]
+
+def greatest(inlist):
+    return max(inlist)
